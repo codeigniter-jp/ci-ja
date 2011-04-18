0b7e2b1e66aefbb6bca33cb4ba9a19e41782bb9f
kenjis <kenji@codeigniter.jp>
2011-04-18 19:52:42 +0900

    improve script to check diff

--- _scripts/check-beta-diff.sh
+++ _scripts/check-beta-diff.sh
@@ -5,6 +5,7 @@
 
 dir="_export"
 diff="diff.txt"
+diff_docs="diff.docs.txt"
 
 show_usage() {
   echo " usage: $0 ci-ja_version"
@@ -31,11 +32,16 @@ if [ ! -d $ci_ja_dir ]; then
   exit 1;
 fi
 
+diff -urN $ci_dir/user_guide $ci_ja_dir/user_guide > $diff_docs
+if [ -s $diff_docs ]; then
+  echo "$diff_docs created"
+fi
+
 rm -rf "$ci_ja_dir/user_guide_ja"
 rm -rf "$ci_ja_dir/system/language/japanese"
 
 diff -urN $ci_dir $ci_ja_dir > $diff
 if [ -s $diff ]; then
-  echo "diff created"
+  echo "$diff created"
 fi
 
